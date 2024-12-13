d9fb6c4be13b427be00f1e71d3ac240b76fb6ba3
Muhammad Rifal Rifdiansyah <119149644+rifal12330@users.noreply.github.com>
2024-12-13 15:24:45 +0700

    Delete app directory

--- app/src/main/java/com/example/projectadvocata/ui/inbox/InboxViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.projectadvocata.ui.inbox
-
-import androidx.lifecycle.ViewModel
-
-class InboxViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/projectadvocata/ui/lawyer/LawyerViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.projectadvocata.ui.lawyer
-
-import androidx.lifecycle.ViewModel
-
-class LawyerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
